1e8a9131757035433d30ed98c08d0d9e254344a7
Viktoria-Murko <tori.kondratuk@gmail.com>
2023-06-11 16:25:31 +0200

    image gallery created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<li class="gallery__item">
+     <a href="${original}" class="gallery__link">
+     <img src="${preview}" 
+     alt="${description}" 
+     data-source="${original}" 
+     class="gallery__image"></a></li>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", handleGalleryContainerClick);
+
+function handleGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryImgEl = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImgEl) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+
+  if (instance.show) {
+    galleryContainer.addEventListener("keydown", handleEscKeyPress);
+  } else {
+    galleryContainer.removeEventListener("keydown", handleEscKeyPress);
+  }
+
+  function handleEscKeyPress(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<li class="gallery__item">
+     <a class="gallery__link" href="${original}" >
+     <img class="gallery__image" src="${preview}"
+     alt="${description}"/></a></li>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
